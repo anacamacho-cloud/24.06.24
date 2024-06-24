945ac75a7d87a5eec99b3cb329ac45321efb0eed
ana.camacho <129320167+anacamacho-cloud@users.noreply.github.com>
2024-06-24 12:10:21 -0300

    Create insert_nome.sql

---
+++ insert_nome.sql
@@ -0,0 +1,6 @@
+insert into `tbl_pessoas` (`nome`) values ('Alfredo');
+insert into `tbl_pessoas` (`nome`) values ('Beto');
+insert into `tbl_pessoas` (`nome`) values ('Charles');
+insert into `tbl_pessoas` (`nome`) values ('Dionisio');
+insert into `tbl_pessoas` (`nome`) values ('Eufrates');
+insert into `tbl_pessoas` (`nome`) values ('Franklin');
